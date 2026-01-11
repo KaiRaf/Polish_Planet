957d430a570a596890268726f465ddfffef74f54
taco-cat-c <bruno.henkels@gmail.com>
2026-01-11 13:43:10 -0500

    Add Main Menu and Pause Menu

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private string mainsceneName;
+    [SerializeField] private string creditSceneName;
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene(mainsceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+  public void LoadCreditsScene()
+    {
+        SceneManager.LoadScene(creditSceneName);
+    }
+
+    public void BackButton()
+    {
+        SceneManager.LoadScene("bruno_scene");
+    }
+}
