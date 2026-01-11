1b2601b291c5553a42f7ca21414de2f155951f76
Nicholas Roy <nicholas@patroy.ca>
2026-01-10 22:41:35 -0500

    billboarding script

---
+++ Assets/Scripts/sprite_billboard.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sprite_billboard : MonoBehaviour
+{
+
+    [SerializeField] bool freezeXZAxis = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (freezeXZAxis)
+        {
+            transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
+        } else
+        {
+            transform.rotation = Camera.main.transform.rotation;
+        }
+        
+    }
+}
