c448f63f58631ec28d4aa6234b37378775d13db3
DPerrotta06 <6225349@edu.vaniercollege.qc.ca>
2026-01-11 14:30:26 -0500

    Clicking logic tweaked
    I had a small error in my code should be better now.

--- Assets/Scripts/PlanetPhase.cs
+++ Assets/Scripts/PlanetPhase.cs
@@ -4,20 +4,18 @@ using UnityEngine.UI;
 public class PlanetPhase : MonoBehaviour
 {
     public Text ProgressPercentage;
+    private int counter = 0;
+
     public enum Phase
     {
         GarbageCleaning, ResourceRepairing, Completed 
     }
 
     public Phase currentPhase = Phase.GarbageCleaning; //starting phase
-
     [Header("Progress")]
     public Slider progressBar;
-
     [Header("Clean the trash")]
     public int phase1Clicks = 350;
-    private int counter = 0;
-
     [Header("Repair the resources")]
     public int phase2Clicks = 1000;
                 
@@ -29,10 +27,12 @@ public class PlanetPhase : MonoBehaviour
 
     public void OnPlanetClicked()
     {
+
         if (currentPhase == Phase.GarbageCleaning)
         {
             counter++;
-            progressBar.value = (float)counter / phase1Clicks;
+            progressBar.value = counter;
+            UpdateProgressText();
             if (counter >= phase1Clicks)
             {
                 SetPhase2();
@@ -41,7 +41,8 @@ public class PlanetPhase : MonoBehaviour
         else if (currentPhase == Phase.ResourceRepairing)
         {
             counter++;
-            progressBar.value = (float)counter / phase2Clicks;
+            progressBar.value = counter;
+            UpdateProgressText();
             if (counter >= phase2Clicks)
             {
                 SetCompleted();
@@ -54,6 +55,8 @@ public class PlanetPhase : MonoBehaviour
         currentPhase = Phase.GarbageCleaning;
         counter = 0;
         progressBar.value = 0;
+        progressBar.maxValue = phase1Clicks;
+        UpdateProgressText();
     }
 
     void SetPhase2()
@@ -61,6 +64,8 @@ public class PlanetPhase : MonoBehaviour
         currentPhase = Phase.ResourceRepairing;
         counter = 0;
         progressBar.value = 0;
+        progressBar.maxValue = phase2Clicks;
+        UpdateProgressText();
     }
 
     void SetCompleted()
@@ -72,7 +77,7 @@ public class PlanetPhase : MonoBehaviour
     void UpdateProgressText()
     {
         float percent = (progressBar.value / progressBar.maxValue) * 100f;
-                ProgressPercentage.text = Mathf.RoundToInt(percent) + "%";
+        ProgressPercentage.text = Mathf.RoundToInt(percent) + "%";
     }
 
     // Update is called once per frame
